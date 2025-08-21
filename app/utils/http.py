8460d1caebda4c6c5452b20466d7d813f18d0751
scrowley194 <scrowley194@gmail.com>
2025-08-21 17:41:58 +0100

    Delete app/utils/http.py

--- app/utils/http.py
+++
@@ -1,6 +0,0 @@
-import requests
-class PoliteSession(requests.Session):
-    def __init__(self, user_agent: str | None = None):
-        super().__init__()
-        if user_agent:
-            self.headers.update({"User-Agent": user_agent})
